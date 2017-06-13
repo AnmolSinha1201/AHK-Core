9b0f33713fa8070de31e6ec3859a8f783f6018c0
Anmol Sinha <anmolsinha1201@outlook.com>
2017-06-14 00:12:23 +0530

    Added visitor placeholder and a default visitor

--- source/Parser/Block.cs
+++ source/Parser/Block.cs
@@ -5,10 +5,16 @@ namespace AHKCore
 {
 	partial class Parser
 	{
+		public BaseVisitor visitor;
 		public string parse(string code)
 		{
-            int i = 0;
+			if (visitor == null)
+				visitor = new defaultVisitor();
+			
+			int i = 0;
 			return complexVariable("this.qwe.asd.qwefunc [ \" qwe \" ]", ref i);
 		}
 	}
+
+	class defaultVisitor : BaseVisitor {}
 }
