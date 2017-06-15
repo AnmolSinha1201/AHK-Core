5da34f2ea61d009eb0cf64f8c31773d04f507026
Anmol Sinha <anmolsinha1201@outlook.com>
2017-06-15 16:18:11 +0530

    Changed behavior of complexFunctionCallClass to a more generalised function chaining

--- source/Visitor/FunctionVisitor.cs
+++ source/Visitor/FunctionVisitor.cs
@@ -32,39 +32,37 @@ namespace AHKCore
 		public class complexFunctionCallClass
 		{
 			public string defaultValue, _this;
-			public List<object> functionParameterList, chain;
-			// type = string for func(param) and List<object> for class["name"](param)
-			public object functionName; 
+			public List<object> functionParameterList, chain, functionChain;
 			
 			public complexFunctionCallClass(string _this, List<object> varOrFuncChain, List<object> functionParameterList)
 			{
 				this._this = _this;
+				functionChain = new List<object>();
 				
 				if (varOrFuncChain[varOrFuncChain.Count - 1].GetType() == typeof(functionCallClass))
 				{
 					var temp = (functionCallClass)varOrFuncChain[varOrFuncChain.Count - 1];
-					this.functionName = temp.functionName;
+					functionChain.Add(temp.functionName);
 					this.functionParameterList = temp.functionParameterList;
 					varOrFuncChain.RemoveAt(varOrFuncChain.Count - 1);
 				}
 				else
 				{
 					int i = 0;
-					functionName = new List<object>();
 
 					/*
 						no need to check for i = 0. dotUnwrap can only be present after i = 1 (example : a.c["n"])
 						i = 0 means entire chain is the name (example : class["name"])
 					 */
 					for (i = varOrFuncChain.Count - 1; i > 0 && varOrFuncChain[i].GetType() != typeof(dotUnwrapClass); i--);
-					((List<object>)functionName).AddRange(varOrFuncChain.GetRange(i, varOrFuncChain.Count - i));
+					functionChain.AddRange(varOrFuncChain.GetRange(i, varOrFuncChain.Count - i));
 					this.functionParameterList = functionParameterList;
 					varOrFuncChain.RemoveRange(i, varOrFuncChain.Count - i);
 				}
 				this.chain = varOrFuncChain;
 
 				this.defaultValue = $"{_this}{this.chain.FlattenAsChain()}" +
-				$"{(functionName.GetType() == typeof(string) ? functionName : ((List<object>)functionName).FlattenAsChain())} ({this.functionParameterList.FlattenAsFunctionParam()})";
+				$"{functionChain.FlattenAsChain()} ({this.functionParameterList.FlattenAsFunctionParam()})";
 			}
 
 			public override string ToString() => defaultValue;
