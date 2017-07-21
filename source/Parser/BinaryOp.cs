758a3983daa3a1458a70443d3a164c9ca2b505b0
Anmol Sinha <anmolsinha1201@outlook.com>
2017-07-21 15:23:39 +0530

    Added other binaryOPs

--- source/Parser/BinaryOp.cs
+++ source/Parser/BinaryOp.cs
@@ -23,7 +23,69 @@ namespace AHKCore
             int pos = origin;
             string[] ops = {"+", "-", "*", "/", "//", "**"};
             string op;
+            
+            CRLFWS(code, ref pos);
+            if ((op = opChecker(code, ref pos, ops)) == null)
+                return null;
+            WS(code, ref pos);
 
+            var expression = Expression(code, ref pos);
+            if (expression == null)
+                return null;
+
+            origin = pos;
+            return new binaryOpLink(op, expression);
+        }
+
+        binaryOpLink concatOperation(string code, ref int origin)
+        {
+            int pos = origin;
+
+            if (CRLFWS(code, ref pos) == null)
+                return null;
+            
+            if (code.Length < pos + ".".Length) //end of string
+				return null;
+			if (code[pos] != '.') 
+				return null;
+
+            if (WS(code, ref pos) == null)
+                return null;
+            
+            var expression = Expression(code, ref pos);
+            if (expression == null)
+                return null;
+            
+            origin = pos;
+            return new binaryOpLink(".", expression);
+        }
+
+        binaryOpLink logicalOperation(string code, ref int origin)
+        {
+            int pos = origin;
+            string[] ops = {"<", ">", "=", "<=", ">=", "==", "!=", "&&", "||"};
+            string op;
+            
+            CRLFWS(code, ref pos);
+            if ((op = opChecker(code, ref pos, ops)) == null)
+                return null;
+            WS(code, ref pos);
+
+            var expression = Expression(code, ref pos);
+            if (expression == null)
+                return null;
+
+            origin = pos;
+            return new binaryOpLink(op, expression);
+        }
+
+        binaryOpLink bitwiseOperation(string code, ref int origin)
+        {
+            int pos = origin;
+            string[] ops = {"<<", ">>"};
+            string op;
+            
+            CRLFWS(code, ref pos);
             if ((op = opChecker(code, ref pos, ops)) == null)
                 return null;
             WS(code, ref pos);
