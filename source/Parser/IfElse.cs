a3ddf21112b797da0d46031fd8ea92e37061c8e7
Anmol Sinha <anmolsinha1201@outlook.com>
2017-08-22 14:56:00 +0530

    fixed indent

--- source/Parser/IfElse.cs
+++ source/Parser/IfElse.cs
@@ -8,55 +8,55 @@ namespace AHKCore
 {
 	partial class Parser
 	{
-        ifElseBlockClass ifElseBlock(string code, ref int origin)
-        {
-            int pos = origin;
-            var _ifBlock = ifBlock(code, ref pos);
-            if (_ifBlock == null)
-                return null;
-            
-            CRLFWS(code, ref pos);
-            var _elseBlock = elseBlock(code, ref pos); //else block not compulsory
-            
-            origin = pos;
-            return visitor.ifElseBlock(new ifElseBlockClass(_ifBlock, _elseBlock));
-        }
+		ifElseBlockClass ifElseBlock(string code, ref int origin)
+		{
+			int pos = origin;
+			var _ifBlock = ifBlock(code, ref pos);
+			if (_ifBlock == null)
+				return null;
+			
+			CRLFWS(code, ref pos);
+			var _elseBlock = elseBlock(code, ref pos); //else block not compulsory
+			
+			origin = pos;
+			return visitor.ifElseBlock(new ifElseBlockClass(_ifBlock, _elseBlock));
+		}
 
-        ifBlockClass ifBlock(string code, ref int origin)
-        {
-            int pos = origin;
+		ifBlockClass ifBlock(string code, ref int origin)
+		{
+			int pos = origin;
 
-            if (stringMatcher(code, ref pos, "if") == null)
-                return null;
-                
-            WS(code, ref pos);
-            var condition = conditionalBlock(code, ref pos);
-            if (condition == null)
-                return null;
+			if (stringMatcher(code, ref pos, "if") == null)
+				return null;
+				
+			WS(code, ref pos);
+			var condition = conditionalBlock(code, ref pos);
+			if (condition == null)
+				return null;
 
-            CRLFWS(code, ref pos);
-            var body = loopBodyBlock(code, ref pos);
-            if (body == null)
-                return null;
-            
-            origin = pos;
-            return visitor.ifBlock(new ifBlockClass(condition, body));
-        }
+			CRLFWS(code, ref pos);
+			var body = loopBodyBlock(code, ref pos);
+			if (body == null)
+				return null;
+			
+			origin = pos;
+			return visitor.ifBlock(new ifBlockClass(condition, body));
+		}
 
-        elseBlockClass elseBlock(string code, ref int origin)
-        {
-            int pos = origin;
+		elseBlockClass elseBlock(string code, ref int origin)
+		{
+			int pos = origin;
 
-            if (stringMatcher(code, ref pos, "else") == null)
-                return null;
-            
-            CRLFWS(code, ref pos);
-            var body = loopBodyBlock(code, ref pos);
-            if (body == null)
-                return null;
+			if (stringMatcher(code, ref pos, "else") == null)
+				return null;
+			
+			CRLFWS(code, ref pos);
+			var body = loopBodyBlock(code, ref pos);
+			if (body == null)
+				return null;
 
-            origin = pos;
-            return visitor.elseBlock(new elseBlockClass(body));
-        }
+			origin = pos;
+			return visitor.elseBlock(new elseBlockClass(body));
+		}
 	}
 }
