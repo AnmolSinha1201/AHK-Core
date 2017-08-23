2fb4ee1a22ae72431a2c7faa75e6eb4be81e194d
Anmol Sinha <anmolsinha1201@outlook.com>
2017-08-23 15:49:57 +0530

    Added ClassTraverser

---
+++ source/NodeTraverser/ClassTraverser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static AHKCore.Nodes;
+
+namespace AHKCore
+{
+	partial class NodeTraverser
+	{
+		classDeclarationClass classDeclaration(classDeclarationClass context)
+		{
+			for (int i = 0; i < context.classBody.Count; i++)
+				context.classBody[i] = objectDispatcher(context.classBody[i]);
+			return visitor.classDeclaration(context);
+		}
+	}
+}
