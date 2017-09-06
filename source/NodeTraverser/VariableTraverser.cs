f40c7b2f8628e84e0859eeee2010121708e89419
Anmol Sinha <anmolsinha1201@outlook.com>
2017-09-06 11:03:16 +0530

    consistency changes

--- source/NodeTraverser/VariableTraverser.cs
+++ source/NodeTraverser/VariableTraverser.cs
@@ -7,26 +7,26 @@ namespace AHKCore
 {
 	partial class NodeTraverser
 	{
-        variableClass variable(variableClass context)
+		variableClass variable(variableClass context)
 		{
 			return visitor.variable(context);
 		}
 
 		complexVariableClass complexVariable(complexVariableClass context)
 		{
-            for (int i = 0; i < context.chain.Count; i++)
-                context.chain[i] = objectDispatcher(context.chain[i]);
-            
-            for (int i = 0; i < context.chain.Count; i++)
-                context.variableChain[i] = objectDispatcher(context.variableChain[i]);
+			for (int i = 0; i < context.chain.Count; i++)
+				context.chain[i] = objectDispatcher(context.chain[i]);
+			
+			for (int i = 0; i < context.chain.Count; i++)
+				context.variableChain[i] = objectDispatcher(context.variableChain[i]);
 
 			return visitor.complexVariable(context);
 		}
 
 		variableAssignClass variableAssign(variableAssignClass context)
 		{
-            context.variable = complexVariable(context.variable);
-            context.expression = objectDispatcher(context.expression);
+			context.variable = complexVariable(context.variable);
+			context.expression = objectDispatcher(context.expression);
 
 			return visitor.variableAssign(context);
 		}
