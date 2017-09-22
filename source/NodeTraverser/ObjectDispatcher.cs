28a0dafea238b4cb02323672c90f2f5ea5c5fc5f
Anmol Sinha <anmolsinha1201@outlook.com>
2017-09-22 14:14:56 +0530

    objectDispatcher now calls traverser functions instead of visitors.

--- source/NodeTraverser/ObjectDispatcher.cs
+++ source/NodeTraverser/ObjectDispatcher.cs
@@ -7,21 +7,30 @@ namespace AHKCore
 {
 	partial class NodeTraverser
 	{
+		/*
+			- objectDispatcher is for 'object' types where a rule produces multiple types, and we would need to typecheck for possible types.
+			- Instead of typechecking for object types inside a function, this is a common function to achieve the same thing, though with
+				slightly bigger overhead.
+			- Composite types like 'functionDeclaraion' have their own traverser function which calls their sub component's traversers, 
+				so call them instead of calling their visitors directly.
+			- It is preferred to call the traverser function instead of calling visitors directly (even for simple types) just for the sake
+				of consistency.
+		 */
 		object objectDispatcher(object o)
 		{
 			switch (o)
 			{
 				case variableAssignClass v :
-					return visitor.variableAssign(v);
+					return variableAssign(v);
 
 				case classDeclarationClass c :
-					return visitor.classDeclaration(c);
+					return classDeclaration(c);
 
 				case functionDeclarationClass f :
-					return visitor.functionDeclaration(f);
+					return functionDeclaration(f);
 
 				case functionCallClass f :
-					return visitor.functionCall(f);
+					return functionCall(f);
 
 				default :
 					return null;
