d7cb736bb34b0806db38dff47f3efe561ed2dcf6
Anmol Sinha <anmolsinha1201@outlook.com>
2017-09-22 12:11:56 +0530

    Added extraInfo interface

--- source/Nodes/Common.cs
+++ source/Nodes/Common.cs
@@ -24,5 +24,19 @@ namespace AHKCore
 				get;
 			}
 		}
+
+		/*
+			- IExtraInfo exposes extra user parameter of a class.
+			- extraInfo is just a placeholder which can be anything.
+			- Every class of AHKCore should implement this interface, except the classes which can not be subdivided ('break', 'NAME', 'variable').
+			- Value = null if nothing is to be exposed.
+			- Users can use this to attach some extra information to AST nodes which they can later use. Interface simply helps write a filter function.
+			- There is no restriction on type of extraInfo. It can be a simple string, or a complex delegate to be called. Users have to handle their own
+				object and delegates. Be careful if you use multiple types as extraInfo.
+		 */
+		public interface IExtraInfo
+		{
+			object extraInfo {get; set;}
+		}
 	}
 }
