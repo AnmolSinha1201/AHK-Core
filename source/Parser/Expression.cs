7c3e9a709648536cdd8a51aad1c84816c22d1bc1
Anmol Sinha <anmolsinha1201@outlook.com>
2018-04-02 12:37:43 +0530

    Bugfix - complexFunctionCall now preceeds complexVariable

--- source/Parser/Expression.cs
+++ source/Parser/Expression.cs
@@ -7,11 +7,15 @@ namespace AHKCore
 	partial class Parser
 	{
 		/*
+			Order of execution:
+			-	complexFunctionCall -> complexVariable (variable can consume NAME. So either
+				check dotUnwrap content or simply execute complexFunctionCall earlier)
 		 */
 		BaseAHKNode Expression(string code, ref int origin)
 		{
 			return STRING(code, ref origin) ?? NUMBER(code, ref origin) 
-			?? complexVariable(code, ref origin) ?? (BaseAHKNode) complexFunctionCall(code, ref origin);
+			?? complexFunctionCall(code, ref origin) ?? complexVariable(code, ref origin)
+			?? (BaseAHKNode)null;
 		}
 	}
 }
